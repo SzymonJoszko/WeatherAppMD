6a377f971e20e06e784bcc0c8067f42ad4510a13
Szymek <szymonjoszko@gmail.com>
2020-03-16 22:59:08 +0100

    Add UI

--- app/src/main/java/com/example/weatherappmd/MainActivity.kt
+++ app/src/main/java/com/example/weatherappmd/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.weatherappmd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView.setImageResource(R.drawable.ic_wi_day_cloudy)
+        imageViewThermometer.setImageResource(R.drawable.ic_wi_thermometer)
+        imageViewCelsius.setImageResource(R.drawable.ic_wi_celsius)
+        imageViewBarometer.setImageResource(R.drawable.ic_wi_barometer)
     }
 }
